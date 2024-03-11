fa7b20313ff5f628a0fdb498e1a8ff2c74806c1f
Jorge Rodriguez <jorgeroma2000@gmail.com>
2024-03-11 17:46:25 +0100

    Improve imgUrl search

--- scrap_js/getCat.js
+++ scrap_js/getCat.js
@@ -66,13 +66,26 @@ async function getEveryProduct(categoria) {
 	
 		while (!salir) {
 			
-			await page.evaluate(() => window.scrollTo(0, document.body.scrollHeight));
+			await page.locator('.footer').scrollIntoViewIfNeeded();
+			// await page.evaluate(() => window.scrollTo(0, document.body.scrollHeight));
 			
 			const tags = await page.locator(".product-container > .product-cell").all();
 			
 			for (let tag of tags) { 
 				const title = await tag.locator('.subhead1-r.product-cell__description-name').first().innerText();
-				const imgUrl = await tag.locator('img').first().getAttribute('src');
+				
+				let timeout = 0;
+				let imgUrl = await tag.locator('img').first().getAttribute('src');
+				while (timeout < 5){
+					if (!imgUrl.includes('https://prod')) {
+						await sleep(500);
+						await tag.locator('img').scrollIntoViewIfNeeded();
+						imgUrl = await tag.locator('img').first().getAttribute('src')	;
+					}else{
+						break;
+					}
+					timeout += 1;
+				}
 				const price = (await tag.locator('.product-price').first().innerText()).split('€')[0];
 				
 				// console.log({title: title, price: price, imgUrl: imgUrl});
@@ -82,12 +95,13 @@ async function getEveryProduct(categoria) {
 			const nextButton = await page.locator('.category-detail__next-subcategory').all();
 			if (nextButton.length > 0) {
 				await page.locator('.category-detail__next-subcategory').click();
+				console.log('Siguiente Pagina.');
 			} else {
 				salir = true;
 			}
 			await sleep(500);
 		}
-		console.log('Seccion terminada');
+		console.log('Seccion terminada.');
 		await browser.close(); 
 		
 	} catch (error) {
