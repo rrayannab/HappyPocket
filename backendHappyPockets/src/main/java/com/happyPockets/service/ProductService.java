fbcfeeb590fb047b574d4acd59ac7e727d0d72bf
Eduardo Cantero <eduardojesuscantero04@gmail.com>
2024-05-14 09:55:15 +0200

    ProductService

--- backendHappyPockets/src/main/java/com/happyPockets/service/ProductService.java
+++ backendHappyPockets/src/main/java/com/happyPockets/service/ProductService.java
@@ -1,5 +1,6 @@
 package com.happyPockets.service;
 
+import org.apache.poi.ss.usermodel.*;
 import com.happyPockets.api.model.Product;
 import com.happyPockets.api.model.ShopPrice;
 import org.springframework.stereotype.Service;
@@ -17,24 +18,41 @@ public class ProductService {
     public ProductService(){
         productList = new ArrayList<>();
 
-        Product product1 = new Product(1, "Galletas", "https://www.carrefour.es/galletas",
+        Workbook libro = new Workbook("productos.xlsx");
+
+        Sheet hoja = libro.getSheetAt(0);
+
+        int i = 0;
+
+        for (Row fila : hoja) {
+            int id = i;
+            String name = fila.getCell(0).getStringCellValue();
+
+            String imageLink = fila.getCell(1).getStringCellValue();
+            ShopPrice[] shopPrices;
+
+        }
+
+        /*
+        Product product1 = new Product(1, "Galletas", "https://www.carrefour.es/galletas", new ShopPrice[]{
                 new ShopPrice(1.5, 1.6, 1.7)
-        );
-        Product product2 = new Product(2, "Leche", "https://www.carrefour.es/leche",
+        });
+        Product product2 = new Product(2, "Leche", "https://www.carrefour.es/leche", new ShopPrice[]{
                 new ShopPrice(1.5, 1.6, 1.7)
-        );
-        Product product3 = new Product(3, "Pan", "https://www.carrefour.es/pan",
+        });
+        Product product3 = new Product(3, "Pan", "https://www.carrefour.es/pan", new ShopPrice[]{
                 new ShopPrice(1.5, 1.6, 1.7)
-        );
-        Product product4 = new Product(4, "Cerveza", "https://www.carrefour.es/cerveza",
+        });
+        Product product4 = new Product(4, "Cerveza", "https://www.carrefour.es/cerveza", new ShopPrice[]{
                 new ShopPrice(1.5, 1.6, 1.7)
-        );
-        Product product5 = new Product(5, "Vino", "https://www.carrefour.es/vino",
+        });
+        Product product5 = new Product(5, "Vino", "https://www.carrefour.es/vino", new ShopPrice[]{
                 new ShopPrice(1.5, 1.6, 1.7)
-        );
-        Product product6 = new Product(6, "Queso", "https://www.carrefour.es/queso",
+        });
+        Product product6 = new Product(6, "Queso", "https://www.carrefour.es/queso", new ShopPrice[]{
                 new ShopPrice(1.5, 1.6, 1.7)
-        );
+        });
+         */
 
         productList.addAll(Arrays.asList(product1, product2, product3, product4, product5, product6));
     }
