9372ae7bd8b0fee33b501749f3224bedc745d668
rrayannab <rayan10@uma.es>
2024-06-02 19:20:24 +0200

    minicart testing

--- PROGRAMA/happypocketweb/web/js/cartData.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-
-module.exports = {
-    items: [
-        { item_name: 'Item 1', amount: 1.00, currency_code: 'USD' },
-        { item_name: 'Item 2', amount: 2.34, custom: 'foo' }
-    ]
-};
